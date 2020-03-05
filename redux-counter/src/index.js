ec976be36d6da8c07417eea5bd402ddc34d8c31f
0pippi0 <j.romany91@gmail.com>
2020-03-05 23:16:50 +0900

    <Provider 컴포넌트를 사용하여 리액트 앱에 store 연동하기> Provider 는 react-redux 라이브러리에 내장되어있는, 리액트 앱에 store 를 손쉽게 연동 할 수 있도록 도와주는 컴포넌트
    컴포넌트를 불러온다음에, 연동 할 컴포넌트를 감싸준다음에 Provider 컴포넌트의 props로 store 값을 설정
    
     App 컴포넌트가 store 에 연동

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -6,8 +6,14 @@ import App from './components/App';
 // Redux 관련 불러오기
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Provider } from 'react-redux';
 
 // 스토어 생성
 const store = createStore(reducers);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
