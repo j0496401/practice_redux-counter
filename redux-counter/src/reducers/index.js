52db0441ff1c968c85d235f80e8234af145defe3
0pippi0 <j.romany91@gmail.com>
2020-03-06 02:51:27 +0900

    <리듀서 함수에 더하기, 빼기, 색상변경 구현>
    배열을 바꾸거나, 이런 작업을 하는게, 그렇게 어려운 작업은 아니지만 간단한 작업 하나 하자고 코드를 필요 이상으로 많이 쓴다는 기분이 드는것은 사실

--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -16,6 +16,7 @@ function counter(state = initialState, action) {
     const { counters } = state;
 
     switch(action.type) {
+
         // 카운터를 새로 추가합니다
         case types.CREATE:
             return {
@@ -27,10 +28,51 @@ function counter(state = initialState, action) {
                     }
                 ]
             };
+
         // slice 를 이용하여 맨 마지막 카운터를 제외시킵니다
         case types.REMOVE:
             return {
                 counters: counters.slice(0, counters.length - 1)
+            };
+
+        // action.index 번째 카운터의 number 에 1 을 더합니다.
+        case types.INCREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index), // 0 ~ action.index 사이의 아이템들을 잘라와서 이 자리에 넣는다
+                    {
+                        ...counters[action.index],  // 기존 값은 유지하면서
+                        number: counters[action.index].number + 1   // number 값을 덮어쓴다 
+                    },
+                    ...counters.slice(action.index + 1, counters.length)    // action.index + 1 ~ 마지막까지 잘라온
+
+                ]
+            };
+        
+        // action.index 번째 카운터의 number 에 1 을 뺍니다
+        case types.DECREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number - 1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            };
+
+        // action.index 번째 카운터의 색상을 변경합니다
+        case types.SET_COLOR:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        color: action.color
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
             };
         
         default:
