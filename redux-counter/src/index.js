97377ddc84ef31214b2c40e432f247a4e19fe96f
0pippi0 <j.romany91@gmail.com>
2020-03-05 22:37:38 +0900

    <Store 만들기> Store 는 리덕스에서 가장 핵심적인 인스턴스 이 안에 현재 상태를 내장하고있고, 구독(subscribe)중인 함수들이 상태가 업데이트 될 때 마다 다시 실행되게 해주죠
    redux 에서 createStore 를 불러온다음에 해당 함수의 파라미터로 우리가 아까 만든 리듀서를 넣어주면돼요

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+// Redux 관련 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+// 스토어 생성
+const store = createStore(reducers);
 
 ReactDOM.render(<App />, document.getElementById('root'));
